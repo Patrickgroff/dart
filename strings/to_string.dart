dcd9b9c91a49b624de9c4e5dedbfe8c5a20f94e5
Patrickgroff <128846161+Patrickgroff@users.noreply.github.com>
2023-06-18 19:53:34 -0300

    Delete strings directory

--- strings/to_string.dart
+++
@@ -1,9 +0,0 @@
-//Example of toString()
-void main() { 
-int number = 20;     
-String result = number.toString(); 
-  
-print("Type of number is ${number.runtimeType}");  
-print("Type of result is ${result.runtimeType}");  
-    
-}   
